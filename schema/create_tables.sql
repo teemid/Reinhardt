d671bfad305a525fd0f429a628458839b90662aa
Emil Grunt <emil.grunt@lumenia.no>
2016-10-10 22:58:58 +0200

    Updates schema and adds more inital data

--- schema/create_tables.sql
+++ schema/create_tables.sql
@@ -5,8 +5,16 @@ CREATE TABLE IF NOT EXISTS coffee_products
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(120) NOT NULL,
     price INT NOT NULL,
-    is_extra BOOL NOT NULL DEFAULT 0,
+    doubleable BOOL NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS coffee_extras
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(120) NOT NULL,
+    price INT NOT NULL DEFAULT 0,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS coffee_orders
@@ -21,6 +29,15 @@ CREATE TABLE IF NOT EXISTS coffee_order_items
 (
     order_id INT NOT NULL,
     item_id INT NOT NULL,
+    is_double BOOLEAN NOT NULL DEFAULT 0,
     FOREIGN KEY (order_id) REFERENCES coffee_orders (id),
     FOREIGN KEY (item_id) REFERENCES coffee_products (id)
+);
+
+CREATE TABLE IF NOT EXISTS coffee_order_extras
+(
+    order_id INT NOT NULL,
+    extra_id INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES coffee_orders (id),
+    FOREIGN KEY (extra_id) REFERENCES coffee_extras (id)
 );
--- schema/initial_data.sql
+++ schema/initial_data.sql
@@ -1,4 +1,7 @@
 use coffee_house;
 
-INSERT INTO coffee_products (name, price) VALUES
-    ('Kaffe', 29), ('Espresso', 39), ('Java', 35);
+INSERT INTO coffee_products (name, price, doubleable) VALUES
+    ('Kaffe', 29, DEFAULT), ('Espresso', 39, 1), ('Java', 35, DEFAULT);
+
+INSERT INTO coffee_extras (name, price) VALUES
+    ('Sugerør', DEFAULT), ('Is', DEFAULT), ('Melk', 5), ('Sukker', 5), ('Sjokostrø', 5);
